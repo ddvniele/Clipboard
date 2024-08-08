efba73b0ed79b10de4d11c7d79fc39d19cd8e0f4
ddvniele <ddvniele@gmail.com>
2024-08-08 14:10:50 +0200

    Initial Commit

---
+++ Clipboard/ClipboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipboardApp.swift
+//  Clipboard
+//
+//  Created by dan on 08/08/24.
+//
+
+import SwiftUI
+
+@main
+struct ClipboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
